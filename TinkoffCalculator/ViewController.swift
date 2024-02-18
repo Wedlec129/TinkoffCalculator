bddb4ffa2fa19f3b7b4ed601035f721329aab280
Wedlec129 <boryasokolov@gmail.com>
2024-02-18 23:37:54 +0300

    fix storyboard

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -2,25 +2,24 @@
 //  ViewController.swift
 //  TinkoffCalculator
 //
-//  Created by Борух Соколов on 18.02.2024.
+//  Created by Aleksey Kolesnikov on 25.01.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        
+        print(buttonText)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        
-        guard let buttonText = sender.currentTitle else { return }
-        
-        print(buttonText)
-        
-    }
-    
+
 }
 
