18e83974ff1016755cbc89150fba16ebb7fa7e14
Wedlec129 <boryasokolov@gmail.com>
2024-02-18 23:28:59 +0300

    fix button in storyboard

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        guard let buttonText = sender.titleLabel?.text else { return }
+        guard let buttonText = sender.currentTitle else { return }
         
         print(buttonText)
         
